6cb1755a7b18dbc2925dbaafe85f37d71be6c574
Pritam Sukumar <pritam@avantifellows.org>
2022-07-27 11:12:37 +0200

    Removing secret stuff

--- generate_table/__main__.py
+++ generate_table/__main__.py
@@ -4,9 +4,9 @@ import boto3
 def initialize_db():
     ddb = boto3.resource('dynamodb',
                          endpoint_url='http://localhost:8000',
-                         region_name='ap-south-1',
-                         aws_access_key_id='AKIARUBOPCTS7SQE7J54',
-                         aws_secret_access_key='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')
+                         region_name='REGION',
+                         aws_access_key_id='KEY',
+                         aws_secret_access_key='SECRET_KEY')
 
     return ddb
 
