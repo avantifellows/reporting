30606c2ff684ae6825a9f236aa38dcf413cbb1dc
Pritam Sukumar <pritam@avantifellows.org>
2022-07-28 16:36:55 +0200

    Removed refs to repository

--- app/models/student_quiz_report.py
+++ app/models/student_quiz_report.py
@@ -55,15 +55,15 @@ class StudentQuizReportModel(BaseModel):
 
 
 class StudentQuizReportController():
-    def __init__(self, repository: StudentQuizReportsDB) -> None:
-        self.__repository = repository
+    def __init__(self, student_quiz_reports_db: StudentQuizReportsDB) -> None:
+        self.__student_quiz_reports_db = student_quiz_reports_db
 
     def get_all(self):
-        return self.__repository.get_all()
+        return self.__student_quiz_reports_db.get_all()
 
     def get_student_quiz_report(self, student_id, quiz_id):
         student_id_quiz_id = student_id + "-" + quiz_id
-        return self.__repository.get_student_quiz_report(student_id_quiz_id=student_id_quiz_id)
+        return self.__student_quiz_reports_db.get_student_quiz_report(student_id_quiz_id=student_id_quiz_id)
 
     def create_student_quiz_report(self, student_quiz_report: StudentQuizReportModel, uid=None):
         if (uid == None):
@@ -72,10 +72,10 @@ class StudentQuizReportController():
             " ", "_")
         uid = uid.replace(" ", "_")
         student_quiz_report.id = uid
-        return self.__repository.create_student_quiz_report(student_quiz_report.dict())
+        return self.__student_quiz_reports_db.create_student_quiz_report(student_quiz_report.dict())
 
     def update_student_quiz_report(self, student_quiz_report: StudentQuizReportModel):
-        return self.__repository.update_student_quiz_report(student_quiz_report.dict())
+        return self.__student_quiz_reports_db.update_student_quiz_report(student_quiz_report.dict())
 
     def delete_student_quiz_report(self, uid: str):
-        return self.__repository.delete_student_quiz_report(uid)
+        return self.__student_quiz_reports_db.delete_student_quiz_report(uid)
