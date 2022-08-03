1e204235a4a70f52754dffd34219c34eeb3eaa10
Pritam Sukumar <pritam@avantifellows.org>
2022-08-03 14:51:34 +0200

    Student Quiz Report V0 done

--- app/models/student_quiz_report.py
+++ app/models/student_quiz_report.py
@@ -39,6 +39,7 @@ class CreateStudentQuizReportModel(BaseModel):
     quiz_name: str = Field(..., example='Quiz Name')
     student_id: str = Field(..., example='STUDENT_ID')
     student_name: str = Field(..., example='Student Name')
+    quiz_date: str = Field(..., example='2022-08-03')
     quiz_stats: QuizStats = Field()
     score_details: ScoreDetails = Field()
 
@@ -46,7 +47,30 @@ class CreateStudentQuizReportModel(BaseModel):
 class StudentQuizReportModel(CreateStudentQuizReportModel):
     id: Optional[str] = Field(..., hidden=True)
     
-    
+    # class Config:
+    #     # Field id doesn't need to be passed and should be removed
+    #     schema_extra = {
+    #         "example": {
+    #             "quiz_id": "QUIZ_ID",
+    #             "quiz_name": "Quiz Name",
+    #             "student_id": "STUDENT_ID",
+    #             "student_name": "Student Name",
+    #             "quiz_stats": {
+    #                 "max_score": 100,
+    #                 "average": 56.4,
+    #                 "number_of_students": 72,
+    #                 "highest_marks": 97.5,
+    #                 "num_correc"
+    #             },
+    #             "score_details": {
+    #                 "score": 65.5,
+    #                 "rank": 7,
+    #                 "percentile": 85.4
+    #             }
+    #         }
+    #     }
+
+
 class StudentQuizReportController():
     def __init__(self, student_quiz_reports_db: StudentQuizReportsDB) -> None:
         self.__student_quiz_reports_db = student_quiz_reports_db
@@ -58,7 +82,7 @@ class StudentQuizReportController():
         student_id_quiz_id = student_id + "-" + quiz_id
         return self.__student_quiz_reports_db.get_student_quiz_report(student_id_quiz_id=student_id_quiz_id)
 
-    def create_student_quiz_report(self, report_data: CreateStudentQuizReportModel):
+    def create_student_quiz_report(self, report_data: CreateStudentQuizReportModel, uid=None):
         uid = report_data.student_id + "-" + report_data.quiz_id
         
         uid = uid.replace(" ", "_")
--- app/routers/reports.py
+++ app/routers/reports.py
@@ -1,24 +1,35 @@
 
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi import HTTPException
+
+from models.student_quiz_report import StudentQuizReportController
 
 
 
 class ReportsRouter:
 
-    def __init__(self) -> None:
-        self.templates = Jinja2Templates(directory="templates")
+    def __init__(self, student_quiz_reports_controller: StudentQuizReportController) -> None:
+        self.__student_quiz_reports_controller = student_quiz_reports_controller
+        self._templates = Jinja2Templates(directory="app/templates")
 
     @property
     def router(self):
-        api_router = APIRouter(prefix='/student_quiz_reports', tags=['student_quiz_reports'])
+        api_router = APIRouter(prefix='/reports', tags=['reports'])
         
         @api_router.get('/')
         def index_route():
             return 'Welcome to all reports Index'
 
         @api_router.get('/student_quiz_report/{report_id}')
-        def student_quiz_report(request: Request, report_id: str ):
-            return self.templates.TemplateResponse("item.html", {"request": request, "id": report_id})
+        def student_quiz_report(request: Request, report_id: str):
+            if report_id == None:
+                raise HTTPException(status_code=400, detail='Both student_id annd quiz_id have to be specified')
+            try:
+                report_data = self.__student_quiz_reports_controller.get_student_quiz_report(report_id=report_id)
+            except KeyError:
+                raise HTTPException(status_code=400, detail='No student_quiz_report found')
+            report_data["score_details"]["percentage"] = "{:.2f}".format(report_data["score_details"]["score"] / report_data["score_details"]["max_marks"])
+            return self._templates.TemplateResponse("student_quiz_report.html", {"request": request, "report_data": report_data})
 
         return api_router
