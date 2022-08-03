e4f04f7f768509fed34705ddb1fe1b492beff767
Pritam Sukumar <pritam@avantifellows.org>
2022-08-03 10:55:13 +0200

    Oops

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from internal.db import initialize_db
 from models.student_quiz_report import StudentQuizReportController
 from db.student_quiz_reports_db import StudentQuizReportsDB
 from routers.student_quiz_reports import StudentQuizReportsRouter
+from routers.reports import ReportsRouter
 
 app = FastAPI()
 
@@ -16,8 +17,11 @@ db = initialize_db()
 student_quiz_reports_db = StudentQuizReportsDB(db)
 student_quiz_reports_controller = StudentQuizReportController(student_quiz_reports_db)
 student_quiz_reports_router = StudentQuizReportsRouter(student_quiz_reports_controller)
+reports_router = ReportsRouter(student_quiz_reports_controller)
 
 app.include_router(student_quiz_reports_router.router)
+app.include_router(reports_router.router)
+
 
 
 @app.get('/')
