9f64d3cb7976c143e77d27495d879a6565446365
suryabulusu <teja.surya59@gmail.com>
2022-08-29 16:36:07 +0530

    minor bug in template path

--- app/routers/reports.py
+++ app/routers/reports.py
@@ -23,7 +23,7 @@ class ReportsRouter:
         self, student_quiz_reports_controller: StudentQuizReportController
     ) -> None:
         self.__student_quiz_reports_controller = student_quiz_reports_controller
-        self._templates = Jinja2Templates(directory="app/templates")
+        self._templates = Jinja2Templates(directory="templates")
 
     @property
     def router(self):
