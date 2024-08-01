08b26ebd2ac357a6eba3eebc1fb679f12a01a6e5
Pritam Sukumar <pritamps@gmail.com>
2024-08-01 16:14:40 +0530

    [PPS] Adding PDF conversion

--- app/routers/student_quiz_reports.py
+++ app/routers/student_quiz_reports.py
@@ -1,10 +1,13 @@
+import os
 from collections import OrderedDict
 from typing import Optional, Union
 from urllib.parse import unquote
 
+import requests
 from auth import verify_token
 from db.reports_db import ReportsDB
 from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.security.api_key import APIKeyHeader
 from fastapi.templating import Jinja2Templates
 
@@ -221,7 +224,12 @@ class StudentQuizReportsRouter:
             )
 
         @api_router.get("/student_quiz_report/v2/{session_id}/{user_id}")
-        def student_quiz_report_v2(request: Request, session_id: str, user_id: str):
+        def student_quiz_report_v2(
+            request: Request,
+            session_id: str,
+            user_id: str,
+            format: Union[str, None] = None,
+        ):
             """
             Returns a student quiz report (V2) for a given session ID and user ID.
 
@@ -265,41 +273,32 @@ class StudentQuizReportsRouter:
                     "error.html", {"request": request, "error_data": error_data}
                 )
 
-            return self._templates.TemplateResponse(
+            templateResponse = self._templates.TemplateResponse(
                 "student_quiz_report_v2.html",
                 {"request": request, "report_data": data[0]},
             )
+            if format != "pdf":
+                return templateResponse
+            else:
+                html_content = templateResponse.body
+                html_content = html_content.decode("utf-8")  # Decode bytes to string
 
-            # return data
-            # report_data = {}
-            # report_data["student_name"] = ""
-            # test_id = data[0]["test_id"]
-            # user_id = data[0]["user_id"]
+                # Convert bytes to string if necessary
+                if isinstance(html_content, bytes):
+                    html_content = html_content.decode()
 
-            # report_data["student_id"] = user_id
-            # if "platform" in data[0] and data[0]["platform"] == "quizengine":
-            #     report_data["test_link"] = QUIZ_URL.format(
-            #         quiz_id=test_id, user_id=user_id, api_key=AF_API_KEY
-            #     )
+                # Send HTML to PDF rendering service
+                url = os.getenv("HTML_TO_PDF_SERVER_URL")
+                response = requests.post(url, json={"html": html_content})
+                # Check if the request was successful
 
-            # section_reports = []
-            # overall_performance = {}
-            # for section in data:
-            #     parsed_section_data = _parse_section_data(section)
-            #     if section["section"] == "overall":
-            #         overall_performance = parsed_section_data
-            #         report_data["percentage"] = parsed_section_data["table_data"][
-            #             "Percentage"
-            #         ]
-            #         report_data["test_name"] = section["test_name"]
-            #         report_data["test_date"] = section["start_date"]
-            #     else:
-            #         section_reports.append(parsed_section_data)
-            # report_data["overall_performance"] = overall_performance
-            # report_data["section_reports"] = section_reports
-            # return self._templates.TemplateResponse(
-            #     "student_quiz_report.html",
-            #     {"request": request, "report_data": report_data},
-            # )
+                if response.status_code == 200:
+                    # Return the PDF as a streaming response
+                    return StreamingResponse(
+                        response.iter_content(chunk_size=10240),
+                        media_type="application/pdf",
+                    )
+                else:
+                    return HTMLResponse(content="Error generating PDF", status_code=500)
 
         return api_router
