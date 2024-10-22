4f092ba8953a956c8fb1b6f51470e8059456b249
“Shashank <shashanktyagiji12345@gmail.com>
2024-10-22 18:24:55 +0530

    fixed return type of verify token function

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
 import httpx
-from fastapi import Request, HTTPException, status
+from fastapi import Request
 
 # Portal Backend URL
 VERIFICATION_URL = "https://b93ddkdz0g.execute-api.ap-south-1.amazonaws.com/auth/verify"
@@ -11,26 +11,17 @@ async def verify_token(request: Request) -> bool:
     bearer_prefix = "Bearer "
 
     if not auth_header or not auth_header.startswith(bearer_prefix):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        return False  # Invalid or missing token
 
     token = auth_header[len(bearer_prefix) :]
     headers = {"Authorization": f"Bearer {token}"}
-    print(VERIFICATION_URL)
-    print(token)
+
     async with httpx.AsyncClient() as client:
         response = await client.get(VERIFICATION_URL, headers=headers)
 
     if response.status_code == 200:
         json_response = response.json()
         if "id" in json_response:
-            return token
+            return True  # Token is valid
 
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid token",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+    return False  # Invalid token
